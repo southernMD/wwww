5a45d58e6bec926acf3f4f5e9ed28a419d530c75
southernMD <2483723241@qq.com>
2022-11-14 20:45:57 +0800

    1

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import { simpleGit, CleanOptions } from 'simple-git';
 const git = simpleGit()
 
 const USER = 'southernMD';
-const PASS = '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';
+const PASS = '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';
 const REPO = 'github.com/southernMD/wwww.git';
 
 const remote = `https://${USER}:${PASS}@${REPO}`;
@@ -12,7 +12,7 @@ const remote = `https://${USER}:${PASS}@${REPO}`;
 try {
     git.init().
     add('./*').
-    commit('hhhh').
+    commit('1').
     addRemote('origin', remote).
     push(['--set-upstream', 'origin', 'main'], () => console.log('done'));
 } catch (error) {
